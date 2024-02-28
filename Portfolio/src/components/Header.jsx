dd9fa495999430822f980a69a3a376a17260ce92
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-02-28 21:51:52 +0530

    logo size fix

--- Portfolio/src/components/Header.jsx
+++ Portfolio/src/components/Header.jsx
@@ -4,7 +4,7 @@ import Image from '../assets/logo.svg';
 function Header() {
     return (
         <div className="w-[80%] h-[10%] flex justify-center items-center absolute top-0">
-            <img src={Image} alt="" className="w-[150px]" />
+            <img src={Image} alt="" className="w-[180px]" />
         </div>
     );
 }
