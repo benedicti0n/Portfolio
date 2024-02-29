d13b9c7f7b8a9d5a64c357a21a7f16f4324a64a4
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-02-29 13:00:29 +0530

    new font add (Kode Mono)

--- Portfolio/src/components/Home.jsx
+++ Portfolio/src/components/Home.jsx
@@ -4,16 +4,17 @@ const Home = () => {
     return (
     <>
     <div className='w-[90%]'>
-        <div className='flex flex-col'>
-            <div className='text-3xl p-4'>Hello web cowboy</div>
+        <div className='flex flex-col' >
+            <div className='text-3xl p-3'>Hello everyNyan 🐱🫶🏼</div>
             <div className='text-8xl'>I'm Ash</div>
         </div>
         <div className='flex w-full'>
-            <div className=' text-start w-[60%] p-4 py-10'>Hi! I'm a Full Stack Web Developer based in Kolkata.
-            Everyday I learn, read and practice to create the best interfaces as possible. It's beautiful to have a passion and a work at the same time, don't you think ?</div>
+            <div className=' text-start w-[60%] p-3 py-10'>How are you? Fine shank'you. I wishhh i were a Bird 🐦 <br />
+            I am a Full Stack web developer, who loves sleeping, anime and stunning web pages.
+            </div>
             <div className=' flex justify-center flex-col text-start pl-12'>
-                <div className='mb-4'>If you want my resume</div>
-                <div className='mt-4'>Or have a chat</div>
+                <div className='mb-4'>Resume?(nehi he)</div>
+                <div className='mt-4'>Wanna chat? Reach out to linkedin</div>
             </div>
         </div>
     </div>
